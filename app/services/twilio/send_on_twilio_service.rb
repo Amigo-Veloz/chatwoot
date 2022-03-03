103ede4c2cc5d4d08565db981e7d396ed004274c
Jaime López <loama18@gmail.com>
2022-03-03 14:53:26 -0600

    hardcode message content

--- app/services/twilio/send_on_twilio_service.rb
+++ app/services/twilio/send_on_twilio_service.rb
@@ -25,7 +25,7 @@ class Twilio::SendOnTwilioService < Base::SendOnChannelService
     puts message.attachments.map(&:file_url)
 
     params = {
-      # body: message.content,
+      body: 'message.content',
       from: channel.phone_number,
       to: contact_inbox.source_id,
       media_url: ['https://amiloz-chatwoot-custom.herokuapp.com//rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBWHc9IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--902b3a558b8d4b9136c0ef09326d159f80f46049/Screen%20Shot%202022-03-01%20at%2016.21.55.png']

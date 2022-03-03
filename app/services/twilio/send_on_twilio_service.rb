71eb5a1f908660d14129871c3485a3e179a35449
Jaime López <loama18@gmail.com>
2022-03-03 15:38:05 -0600

    test sending info

--- app/services/twilio/send_on_twilio_service.rb
+++ app/services/twilio/send_on_twilio_service.rb
@@ -16,19 +16,19 @@ class Twilio::SendOnTwilioService < Base::SendOnChannelService
 
   def message_params
     puts 'New Message'
-    puts message.content
+    # puts message.content
 
-    puts 'Attachments'
-    puts message.attachments
+    # puts 'Attachments'
+    # puts message.attachments
 
-    puts 'Message Attachments'
-    puts message.attachments.map(&:file_url)
+    # puts 'Message Attachments'
+    # puts message.attachments.map(&:file_url)
 
     params = {
       body: message.attachments.map(&:file_url),
       from: channel.phone_number,
       to: contact_inbox.source_id
-      # media_url: ['https://amiloz-chatwoot-custom.herokuapp.com//rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBWHc9IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--902b3a558b8d4b9136c0ef09326d159f80f46049/Screen%20Shot%202022-03-01%20at%2016.21.55.png']
+      # media_url: 'https://amiloz-chatwoot-custom.herokuapp.com//rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBWHc9IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--902b3a558b8d4b9136c0ef09326d159f80f46049/Screen%20Shot%202022-03-01%20at%2016.21.55.png'
     }
     # params[:media_url] = attachments if message.attachments.present?
     params
